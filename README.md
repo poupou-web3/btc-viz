d4a6e4d18403d1a564b3fec1fe2431580b6f9824
cypto-viz <btc-viz@protonmail.com>
2020-11-01 15:34:24 +0100

    Deployment commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# Btc-viz
+> A simple Data App to show the some awesome Bitcoin visualization
+
+## Give it a look
+[Btc-viz](https://btc-viz.com)
+
+## Setup
+
+```python
+pip install -r requirements.txt
+streamlit run appBtcViz.py
+```
+
---
+++ appBtcViz.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+@author: btc-viz
+"""
+
+#%% import packages
+import streamlit as st
+
+#%% the GUI
+st.title("Bitcoin visualization")
+
+st.info(
+    """ By: [Btc-viz](https://github.com/crypto-viz) | Code source: [GitHub](https://github.com/crypto-viz/btc-viz) """
+)
